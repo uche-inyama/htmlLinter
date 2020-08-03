492db20b76af373f5ffbcb09ef888651b567fb3f
uche-inyama <depoulet@yahoo.com>
2020-08-03 19:43:12 +0100

    add method for checking src and alt in an img tag & href in an a tag

--- lib/error_helper.rb
+++ lib/error_helper.rb
@@ -1,5 +1,39 @@
 module Checks
-  def check_doctype
+  def check_doctype(file)
     'check Doctype' unless file.include? '<!Doctype html>'    
+  end
+
+  def check_lang(file)
+    'add language tag' unless file.include?(/<html lang=\"en\">/)
+  end
+
+  def check_alt(file)
+    error_message = ''
+    file.each_with_index do |element, index|
+      if element.match(/<img/)
+        error_message << "add alt = \"\"to the img tag on line #{index + 1}" unless element.match(/alt/)
+      end
+    end
+    error_message
+  end
+
+  def check_href(file)
+    error_message = ''
+    file.each_with_index do |element, index|
+      if element.match(/<a/)
+        error_message << "add href to the a tag on line #{index + 1}" unless element.match(/href/)
+      end
+    end
+    error_message
+  end
+
+  def check_src
+    error_message = ''
+    file.each_with_index do |element, index|
+      if element.match(/<img/)
+        error_message << "add src = \"\"to the img tag on line #{index + 1}" unless element.match(/src/)
+      end
+    end
+    error_message
   end
 end
