0d23e6a02fa027fd5685df6cb9233ce6a5300e79
uche-inyama <depoulet@yahoo.com>
2020-08-05 06:39:07 +0100

    implement inheritance

--- bin/main.rb
+++ bin/main.rb
@@ -1,15 +1,13 @@
 # !/usr/bin/env ruby
 require_relative '../lib/module.rb'
+require_relative '../lib/file'
 
 #:nodoc:
-class Linter
+class Linter < CssFile
   include ChecksModule
 
-  attr_accessor :file, :file_content
-
   def initialize(filename)
-    @file_content = File.read(filename)
-    @file = File.read(filename).split(/\n/)
+    super
   end
 
   def checks
---
+++ lib/file.rb
@@ -0,0 +1,10 @@
+class CssFile
+  include ChecksModule
+
+  attr_accessor :file, :file_content
+
+  def initialize(filename)
+    @file_content = File.read(filename)
+    @file = File.read(filename).split(/\n/)
+  end
+end
